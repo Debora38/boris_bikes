0b5dc6a5c4e9b862bb35294feda500ff933faba3
Debora <debora.piu92@gmail.com>
2019-01-11 15:08:16 +0000

    full? and empty? methods added and made private

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -17,11 +17,19 @@ class DockingStation
   end
 
   def dock(bike)
-    if @bike_arr.count >= 20
+    if @bike_arr.full?
       raise "Already full"
     else
       @bike_arr << bike
     end
   end
 
+  private
+  def full?
+    @bike_arr.count >= 20
+  end
+
+  def empty?
+    @bike_arr.count == 0
+  end
 end
